d50baa736b1d4cb9230f97c6d6b8a4c37ece8157
Raj Chandvaniya <rajchandvaniya@outlook.com>
2023-02-03 16:56:03 +0530

    Added loader

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import BackgroundImage from "../../img/bg2.jpg";
 import { v4 as uuidv4 } from "uuid";
 import Form from "./Form";
+import { ColorRing } from "react-loader-spinner";
 import {
   BACKEND_ENDPOINT,
   TRUECALLER_APP_KEY,
@@ -13,35 +14,38 @@ function Body() {
   const [userDetails, setUserDetails] = useState({});
   const [trueCallerNotFound, setTrueCallerNotFound] = useState(false);
   const [uniqueRequestId] = useState(uuidv4());
+  const [showLoader, setShowLoader] = useState(false);
 
   function initiateLogin() {
     window.location.href = `truecallersdk://truesdk/web_verify?requestNonce=${uniqueRequestId}&partnerKey=${TRUECALLER_APP_KEY}&partnerName=Guruhari Darshan Parva&lang=EN`;
-
+    setShowLoader(true);
     setTimeout(async function () {
       if (document.hasFocus()) {
         // Truecaller is not installed or is ios
         setTrueCallerNotFound(true);
       } else {
         for (let i = 0; i < 3 && !isLoggedIn; i++) {
-          let response = await fetch(BACKEND_ENDPOINT + "/" + uniqueRequestId);
-          if (response.ok) {
-            const resp = await response.json();
-            localStorage.setItem(
-              USER_DETAILS_LOCAL_STORAGE_KEY,
-              JSON.stringify(resp)
+          setTimeout(async () => {
+            let response = await fetch(
+              BACKEND_ENDPOINT + "/" + uniqueRequestId
             );
-            setUserDetails(resp);
-            setIsLoggedIn(true);
-            break;
-          } else {
-            if (i == 2) {
-              setTrueCallerNotFound(true);
+            if (response.ok) {
+              const resp = await response.json();
+              localStorage.setItem(
+                USER_DETAILS_LOCAL_STORAGE_KEY,
+                JSON.stringify(resp)
+              );
+              setUserDetails(resp);
+              setIsLoggedIn(true);
+            } else {
+              if (i == 2) {
+                setTrueCallerNotFound(true);
+              }
             }
-            window.alert("Please wait for few seconds...");
-          }
+          }, 1000);
         }
       }
-    }, 1000);
+    }, 3000);
   }
 
   const alreadyLoggedIn = localStorage.getItem(USER_DETAILS_LOCAL_STORAGE_KEY);
@@ -69,12 +73,25 @@ function Body() {
   } else {
     return !isLoggedIn ? (
       <div className="m-3 p-3 py-32 rounded-lg bg-white shadow-xl flex justify-center">
-        <button
-          className="rounded-lg bg-gradient-to-r from-orange-600 to-pink-600 text-white p-3 hover:bg-blue-800 text-bold text-md"
-          onClick={() => initiateLogin()}
-        >
-          Register
-        </button>
+        {!showLoader && (
+          <button
+            className="rounded-lg bg-gradient-to-r from-orange-600 to-pink-600 text-white p-3 hover:bg-blue-800 text-bold text-md"
+            onClick={() => initiateLogin()}
+          >
+            Register
+          </button>
+        )}
+        {showLoader && (
+          <ColorRing
+            visible={true}
+            height="80"
+            width="80"
+            ariaLabel="blocks-loading"
+            wrapperStyle={{}}
+            wrapperClass="blocks-wrapper"
+            colors={["#ea580c", "#db2777", "#ea580c", "#db2777", "#db2777"]}
+          />
+        )}
       </div>
     ) : (
       <Form
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { BACKEND_ENDPOINT, USER_DETAILS_LOCAL_STORAGE_KEY } from "../constants";
+import { ColorRing } from "react-loader-spinner";
 
 function Form({ trueCallerNotFound, name, phoneNumber }) {
   const [fullName, setFullName] = useState(name);
@@ -15,6 +16,8 @@ function Form({ trueCallerNotFound, name, phoneNumber }) {
   const [serverError, setServerError] = useState("");
   const [done, setDone] = useState(false);
 
+  const [showLoader, setShowLoader] = useState(false);
+
   async function submit() {
     if (fullName != "" && mobileNumber.length === 10) {
       console.log("Making the request");
@@ -25,6 +28,7 @@ function Form({ trueCallerNotFound, name, phoneNumber }) {
         reference: reference,
       };
       console.log(body);
+      setShowLoader(true);
       const response = await fetch(BACKEND_ENDPOINT + "/attendance", {
         method: "POST",
         mode: "cors",
@@ -35,6 +39,7 @@ function Form({ trueCallerNotFound, name, phoneNumber }) {
       });
       if (response.status != 200) {
         setServerError(await response.text());
+        setShowLoader(false);
       } else {
         localStorage.setItem(
           USER_DETAILS_LOCAL_STORAGE_KEY,
@@ -61,161 +66,181 @@ function Form({ trueCallerNotFound, name, phoneNumber }) {
     );
   } else
     return (
-      <div className="p3 m-3">
-        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-          {trueCallerNotFound && (
-            <>
-              <div
-                className="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4"
-                role="alert"
-              >
-                <p>Truecaller not found or you have iphone.</p>
-                <p>Please fill the below form.</p>
-              </div>
-              <br />
-            </>
-          )}
-
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2">
-              Full Name
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="fullname"
-              type="text"
-              onChange={(e) => {
-                setNameValidationError(false);
-                setFullName(e.target.value);
-              }}
-              value={fullName}
-              disabled={!trueCallerNotFound}
-            />
-            {nameValdationError ? (
-              <span className="m-2 text-red-500 text-sm">
-                Name should't be blank
-              </span>
-            ) : (
-              <></>
-            )}
-          </div>
-
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2">
-              Mobile Number
-            </label>
-
-            <input
-              className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-              id="mobilenumber"
-              type="text"
-              onChange={(e) => {
-                setMobileNumberValidationError(false);
-                setMobileNumber(e.target.value);
-              }}
-              value={mobileNumber}
-              disabled={!trueCallerNotFound}
-            />
-            {mobileNumberValidationError ? (
-              <span className="text-red-500 text-sm">
-                Mobile Number should be 10 digits long
-              </span>
-            ) : (
-              <></>
-            )}
-          </div>
-
-          <p className="block text-gray-700 text-sm font-bold mb-2">Sabha</p>
-          <div className="flex items-center">
-            <input
-              id="santacruz"
-              type="radio"
-              value=""
-              name="sabha-radio"
-              className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-              onChange={(e) => setSabha("Santacruz")}
-            />
-            <label className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-              Santacruz
-            </label>
-          </div>
-          <div className="flex items-center">
-            <input
-              id="andheri"
-              type="radio"
-              value=""
-              name="sabha-radio"
-              className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-              onChange={(e) => setSabha("Andheri")}
-            />
-            <label className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-              Andheri
-            </label>
-          </div>
-          <div className="flex items-center mb-4">
-            <input
-              id="parla"
-              type="radio"
-              value=""
-              name="sabha-radio"
-              className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-              onChange={(e) => setSabha("Vile Parle")}
-            />
-            <label className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-              Vile Parle
-            </label>
-          </div>
-
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2">
-              Reference (Optional)
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="reference"
-              type="text"
-              placeholder=""
-              onChange={(e) => {
-                setReference(e.target.value);
-              }}
+      <>
+        {showLoader && (
+          <div className="p-3 m-3 bg-white rounded flex justify-center py-40">
+            <ColorRing
+              visible={true}
+              height="80"
+              width="80"
+              ariaLabel="blocks-loading"
+              wrapperStyle={{}}
+              wrapperClass="blocks-wrapper"
+              colors={["#ea580c", "#db2777", "#ea580c", "#db2777", "#db2777"]}
             />
           </div>
-
-          <div className="flex items-center justify-between">
-            <button
-              className="bg-gradient-to-r from-orange-600 to-pink-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="button"
-              onClick={submit}
-            >
-              Submit
-            </button>
-          </div>
-
-          {serverError != "" ? (
-            <div
-              className="my-4 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
-              role="alert"
-            >
-              <span className="block sm:inline">{serverError}</span>
-              <span className="absolute top-0 bottom-0 right-0 px-4 py-3">
-                <button onClick={() => setServerError("")}>
-                  <svg
-                    className="fill-current h-6 w-6 text-red-500"
-                    role="button"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
+        )}
+        {!showLoader && (
+          <div className="p3 m-3">
+            <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+              {trueCallerNotFound && (
+                <>
+                  <div
+                    className="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4"
+                    role="alert"
                   >
-                    <title>Close</title>
-                    <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
-                  </svg>
+                    <p>Truecaller not found or you have iphone.</p>
+                    <p>Please fill the below form.</p>
+                  </div>
+                  <br />
+                </>
+              )}
+
+              <div className="mb-4">
+                <label className="block text-gray-700 text-sm font-bold mb-2">
+                  Full Name
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="fullname"
+                  type="text"
+                  onChange={(e) => {
+                    setNameValidationError(false);
+                    setFullName(e.target.value);
+                  }}
+                  value={fullName}
+                  disabled={!trueCallerNotFound}
+                />
+                {nameValdationError ? (
+                  <span className="m-2 text-red-500 text-sm">
+                    Name should't be blank
+                  </span>
+                ) : (
+                  <></>
+                )}
+              </div>
+
+              <div className="mb-4">
+                <label className="block text-gray-700 text-sm font-bold mb-2">
+                  Mobile Number
+                </label>
+
+                <input
+                  className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                  id="mobilenumber"
+                  type="text"
+                  onChange={(e) => {
+                    setMobileNumberValidationError(false);
+                    setMobileNumber(e.target.value);
+                  }}
+                  value={mobileNumber}
+                  disabled={!trueCallerNotFound}
+                />
+                {mobileNumberValidationError ? (
+                  <span className="text-red-500 text-sm">
+                    Mobile Number should be 10 digits long
+                  </span>
+                ) : (
+                  <></>
+                )}
+              </div>
+
+              <p className="block text-gray-700 text-sm font-bold mb-2">
+                Sabha
+              </p>
+              <div className="flex items-center">
+                <input
+                  id="santacruz"
+                  type="radio"
+                  value=""
+                  name="sabha-radio"
+                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                  onChange={(e) => setSabha("Santacruz")}
+                />
+                <label className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                  Santacruz
+                </label>
+              </div>
+              <div className="flex items-center">
+                <input
+                  id="andheri"
+                  type="radio"
+                  value=""
+                  name="sabha-radio"
+                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                  onChange={(e) => setSabha("Andheri")}
+                />
+                <label className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                  Andheri
+                </label>
+              </div>
+              <div className="flex items-center mb-4">
+                <input
+                  id="parla"
+                  type="radio"
+                  value=""
+                  name="sabha-radio"
+                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                  onChange={(e) => setSabha("Vile Parle")}
+                />
+                <label className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                  Vile Parle
+                </label>
+              </div>
+
+              <div className="mb-4">
+                <label className="block text-gray-700 text-sm font-bold mb-2">
+                  Reference (Optional)
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="reference"
+                  type="text"
+                  placeholder=""
+                  onChange={(e) => {
+                    setReference(e.target.value);
+                  }}
+                />
+              </div>
+
+              <div className="flex items-center justify-between">
+                <button
+                  className="bg-gradient-to-r from-orange-600 to-pink-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                  type="button"
+                  disabled={showLoader}
+                  onClick={submit}
+                >
+                  Submit
                 </button>
-              </span>
-            </div>
-          ) : (
-            <></>
-          )}
-        </form>
-      </div>
+              </div>
+
+              {serverError != "" ? (
+                <div
+                  className="my-4 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+                  role="alert"
+                >
+                  <span className="block sm:inline">{serverError}</span>
+                  <span className="absolute top-0 bottom-0 right-0 px-4 py-3">
+                    <button onClick={() => setServerError("")}>
+                      <svg
+                        className="fill-current h-6 w-6 text-red-500"
+                        role="button"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20"
+                      >
+                        <title>Close</title>
+                        <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+                      </svg>
+                    </button>
+                  </span>
+                </div>
+              ) : (
+                <></>
+              )}
+            </form>
+          </div>
+        )}
+      </>
     );
 }
 
